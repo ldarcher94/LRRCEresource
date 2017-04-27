c778af459237e1fcb29f9870ab92663c5feaf032
Luke Archer <ld.archer94@gmail.com>
2017-04-27 04:43:16 +0100

    Change button design
    Added directionality to gene images

--- catalog/templatetags/catalog_extras.py
+++ catalog/templatetags/catalog_extras.py
@@ -1,12 +1,10 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 
+
 register = template.Library()
    
 @register.filter
 def filter_by_taxonomy(Organism, taxonomic_group):
     return Organism.filter(taxonomic_group=taxonomic_group)
 
-@register.filter(name='fasta_length')
-def fasta_length(sequence):
-    return get_fasta_length(sequence)
